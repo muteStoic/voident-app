e32119fd5cc6241d7bca208e13034ad87014a685
muteStoic <50288669+muteStoic@users.noreply.github.com>
2024-07-23 01:52:02 +0800

    12

--- pages/main_Editor.py
+++ pages/main_Editor.py
@@ -4,5 +4,8 @@ st.title("Void Entertainment Login")
 st.text_area(
     "Main Editor" 
     ,value = "(Place content in the container to post it to website)"
-    ,height = 300
+    ,height = 400
 )
+
+
+st.text_input("Post Title")
