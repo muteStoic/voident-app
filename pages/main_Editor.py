fec50e526b5e25a79c4171e54b12f8943df121dd
muteStoic <50288669+muteStoic@users.noreply.github.com>
2024-07-23 01:49:29 +0800

    2

--- pages/main_Editor.py
+++ pages/main_Editor.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Void Entertainment Login")
+st.title("Void Entertainment Login 2")
 st.text_area(
     "Main Editor" 
     ,value = "(Place content in the container below to post it to website)"
